3344cc3644984da8fb6df268cc29d7cdd08ec60b
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-09 09:25:18 +0100

    feat: Equal Sides of an Array
    Add Equal Sides of an Array kata

---
+++ kata/6-kyu/equal-sides-of-an-array/index.ts
@@ -0,0 +1,17 @@
+function sumArr(arr: number[]): number {
+  return arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+}
+
+function findEvenIndex(arr: number[]): number {
+  return arr.findIndex((_, index) => {
+    const leftSum = sumArr(arr.slice(0, index));
+    const rightSum = sumArr(arr.slice(index + 1));
+
+    return leftSum === rightSum;
+  });
+}
+
+export default findEvenIndex;
