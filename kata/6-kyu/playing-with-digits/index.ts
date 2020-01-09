1e5efad30b117d613194735e00e6fd1da27077dc
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-09 08:21:32 +0100

    feat: playing with digits
    Add playing with digits kata

---
+++ kata/6-kyu/playing-with-digits/index.ts
@@ -0,0 +1,16 @@
+function digPow(n: number, p: number): number {
+  const digits = n
+    .toString()
+    .split("")
+    .map(Number);
+
+  const sum = digits.reduce(
+    (accumulator, currentValue, index) =>
+      accumulator + currentValue ** (index + p),
+    0
+  );
+
+  return sum % n ? -1 : sum / n;
+}
+
+export default digPow;
