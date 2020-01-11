c88b725990adc6d76266dd1a07346e7ed792c779
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-11 23:35:32 +0000

    feat: Draw stairs
    Add draw stairs kata

---
+++ kata/8-kyu/draw-stairs/index.ts
@@ -0,0 +1,11 @@
+function drawStairs(n: number): string {
+  const stair = [];
+
+  for (let i = 0; i < n; i += 1) {
+    stair[i] = `${" ".repeat(i)}I`;
+  }
+
+  return stair.join("\n");
+}
+
+export default drawStairs;
