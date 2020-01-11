fab5b49949f59ddaf0c208a4dcfd08ad169c8366
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-11 23:12:22 +0000

    feat: Detect palindrome
    Add Detect palindrome kata

---
+++ kata/beta/detect-palindrome/index.ts
@@ -0,0 +1,13 @@
+function isPalindrome(str: string): boolean {
+  const characters = str.toLowerCase().replace(/\W/g, "");
+
+  return (
+    characters ===
+    characters
+      .split("")
+      .reverse()
+      .join("")
+  );
+}
+
+export default isPalindrome;
