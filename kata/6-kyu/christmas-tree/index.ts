cf438f66bf7ebc8bc0bacef5f9f82bd21af58e86
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-15 19:59:11 +0100

    feat: 'Christmas Tree'
    Add 'Christmas Tree' kata

---
+++ kata/6-kyu/christmas-tree/index.ts
@@ -0,0 +1,24 @@
+function christmasTree(height: number): string {
+  const rows = height;
+  const columns = height * 2 - 1;
+  const midpoint = Math.floor((height * 2 - 1) / 2);
+  const tree = [];
+
+  for (let row = 0; row < rows; row += 1) {
+    const level = [];
+
+    for (let column = 0; column < columns; column += 1) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level.push("*");
+      } else {
+        level.push(" ");
+      }
+    }
+
+    tree.push(level.join(""));
+  }
+
+  return tree.join("\n");
+}
+
+export default christmasTree;
