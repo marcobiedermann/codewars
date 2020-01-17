7962a1a68ffd8440da5f3003fb626492510c8ff6
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-17 08:16:28 +0100

    feat: 'A Needle in the Haystack'
    Add 'A Needle in the Haystack' kata

---
+++ kata/8-kyu/a-needle-in-the-haystack/index.ts
@@ -0,0 +1,7 @@
+function findNeedle(haystack: any[]): string {
+  const position = haystack.indexOf("needle");
+
+  return `found the needle at position ${position}`;
+}
+
+export default findNeedle;
