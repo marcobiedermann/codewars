bf7b51fdc7609bf6aa11dce18fab176249afd8c8
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-17 08:22:55 +0100

    feat: 'A Needle in the Haystack'
    Add 'A Needle in the Haystack' kata

---
+++ kata/8-kyu/a-needle-in-the-haystack/index.test.ts
@@ -0,0 +1,79 @@
+import findNeedle from ".";
+
+describe("findNeedle", () => {
+  it("should find `needle` in a haystack", () => {
+    expect(
+      findNeedle([
+        "3",
+        "123124234",
+        undefined,
+        "needle",
+        "world",
+        "hay",
+        2,
+        "3",
+        true,
+        false
+      ])
+    ).toEqual("found the needle at position 3");
+    expect(
+      findNeedle([
+        "283497238987234",
+        "a dog",
+        "a cat",
+        "some random junk",
+        "a piece of hay",
+        "needle",
+        "something somebody lost a while ago"
+      ])
+    ).toEqual("found the needle at position 5");
+    expect(
+      findNeedle([
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        8,
+        7,
+        5,
+        4,
+        3,
+        4,
+        5,
+        6,
+        67,
+        5,
+        5,
+        3,
+        3,
+        4,
+        2,
+        34,
+        234,
+        23,
+        4,
+        234,
+        324,
+        324,
+        "needle",
+        1,
+        2,
+        3,
+        4,
+        5,
+        5,
+        6,
+        5,
+        4,
+        32,
+        3,
+        45,
+        54
+      ])
+    ).toEqual("found the needle at position 30");
+  });
+});
