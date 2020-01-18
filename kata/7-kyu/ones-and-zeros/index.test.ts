0364f8ded8af77469457696f91d5eb2321f9183a
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-18 11:02:19 +0100

    feat: 'Ones and Zeros'
    Add 'Ones and Zeros' kata

---
+++ kata/7-kyu/ones-and-zeros/index.test.ts
@@ -0,0 +1,10 @@
+import binaryArrayToNumber from ".";
+
+describe("binaryArrayToNumber", () => {
+  it("should convert binary value in array to integer", () => {
+    expect(binaryArrayToNumber([0, 0, 0, 1])).toEqual(1);
+    expect(binaryArrayToNumber([0, 0, 1, 0])).toEqual(2);
+    expect(binaryArrayToNumber([1, 1, 1, 1])).toEqual(15);
+    expect(binaryArrayToNumber([0, 1, 1, 0])).toEqual(6);
+  });
+});
---
+++ kata/7-kyu/ones-and-zeros/index.ts
@@ -0,0 +1,5 @@
+function binaryArrayToNumber(arr: number[]): number {
+  return parseInt(arr.join(""), 2);
+}
+
+export default binaryArrayToNumber;
