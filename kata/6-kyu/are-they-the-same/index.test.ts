d375cb851dafb4152b0506d44126fa5289a552e6
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-20 08:28:19 +0100

    feat: 'Are they the same?'
    Add 'Are they the same?' kata

---
+++ kata/6-kyu/are-they-the-same/index.test.ts
@@ -0,0 +1,21 @@
+import comp from ".";
+
+describe("comp", () => {
+  it("should check if both array are the same with the same multiplicities", () => {
+    expect(
+      comp(
+        [121, 144, 19, 161, 19, 144, 19, 11],
+        [
+          11 * 11,
+          121 * 121,
+          144 * 144,
+          19 * 19,
+          161 * 161,
+          19 * 19,
+          144 * 144,
+          19 * 19
+        ]
+      )
+    ).toEqual(true);
+  });
+});
---
+++ kata/6-kyu/are-they-the-same/index.ts
@@ -0,0 +1,14 @@
+function comp(array1: number[], array2: number[]): boolean {
+  if (!Array.isArray(array1) || !Array.isArray(array2)) {
+    return false;
+  }
+
+  const sortedArray1 = array1.sort((a, b) => a - b);
+  const sortedArray2 = array2.sort((a, b) => a - b);
+
+  return sortedArray1
+    .map(element => element ** 2)
+    .every((element, index) => element === sortedArray2[index]);
+}
+
+export default comp;
