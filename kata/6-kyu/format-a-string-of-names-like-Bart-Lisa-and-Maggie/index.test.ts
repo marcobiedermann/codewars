39224cbb51c1ca2292d99afe0577342d14de0e37
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-20 08:52:30 +0100

    feat: 'Format a string of names like 'Bart, Lisa & Maggie'.'
    Add 'Format a string of names like 'Bart, Lisa & Maggie'.' kata

---
+++ kata/6-kyu/format-a-string-of-names-like-Bart-Lisa-and-Maggie/index.test.ts
@@ -0,0 +1,21 @@
+import list from ".";
+
+describe("list", () => {
+  it("should separate names by comma, except for the last two", () => {
+    expect(
+      list([
+        { name: "Bart" },
+        { name: "Lisa" },
+        { name: "Maggie" },
+        { name: "Homer" },
+        { name: "Marge" }
+      ])
+    ).toEqual("Bart, Lisa, Maggie, Homer & Marge");
+    expect(
+      list([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }])
+    ).toEqual("Bart, Lisa & Maggie");
+    expect(list([{ name: "Bart" }, { name: "Lisa" }])).toEqual("Bart & Lisa");
+    expect(list([{ name: "Bart" }])).toEqual("Bart");
+    expect(list([])).toEqual("");
+  });
+});
---
+++ kata/6-kyu/format-a-string-of-names-like-Bart-Lisa-and-Maggie/index.ts
@@ -0,0 +1,12 @@
+interface Person {
+  name: string;
+}
+
+function list(names: Person[]): string {
+  return names
+    .map(({ name }) => name)
+    .join(", ")
+    .replace(/(.*), (.*)/, "$1 & $2");
+}
+
+export default list;
