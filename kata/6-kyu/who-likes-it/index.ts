a744ce20f32d12b1262b9e46c9e1376371c79564
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-23 09:10:10 +0100

    refactor: index
    Calculate index dynamically

--- kata/6-kyu/who-likes-it/index.ts
+++ kata/6-kyu/who-likes-it/index.ts
@@ -7,7 +7,7 @@ function likes(names: string[]): string {
     `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
   ];
 
-  const index = Math.min(names.length, 4);
+  const index = Math.min(names.length, templates.length - 1);
 
   return templates[index];
 }
