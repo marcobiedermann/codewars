a556441126b9137904a1a8e110a355a259ce8cd1
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-24 08:24:21 +0100

    feat: 'Replace With Alphabet Position'
    Add 'Replace With Alphabet Position' kata

---
+++ kata/6-kyu/replace-with-alphabet-position/index.test.ts
@@ -0,0 +1,12 @@
+import alphabetPosition from '.';
+
+describe('alphabetPosition', () => {
+  it('should replace every letter with its position in the alphabet', () => {
+    expect(alphabetPosition("The sunset sets at twelve o' clock.")).toEqual(
+      '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11',
+    );
+    expect(alphabetPosition('The narwhal bacons at midnight.')).toEqual(
+      '20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20',
+    );
+  });
+});
