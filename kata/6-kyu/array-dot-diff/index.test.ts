256249342982f7c49f7b0abfeae005a806b1ba6e
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-25 13:50:17 +0100

    feat: 'Array.diff'
    Add 'Array.diff' kata

---
+++ kata/6-kyu/array-dot-diff/index.test.ts
@@ -0,0 +1,9 @@
+import array_diff from '.';
+
+describe('array_diff', () => {
+  it('should remove all values from list `a` which are present in list `b`', () => {
+    expect(array_diff([], [4, 5])).toEqual([]);
+    expect(array_diff([3, 4], [3])).toEqual([4]);
+    expect(array_diff([1, 8, 2], [])).toEqual([1, 8, 2]);
+  });
+});
---
+++ kata/6-kyu/array-dot-diff/index.ts
@@ -0,0 +1,5 @@
+function array_diff(a: number[], b: number[]): number[] {
+  return a.filter(val => !b.includes(val));
+}
+
+export default array_diff;
