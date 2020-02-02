1715d7f398bd2ca9cb1e060144752bd66161f786
Marco Biedermann <marco.biedermann@gmx.de>
2020-02-02 12:02:49 +0100

    docs: 'Counting sheep...'
    Add 'Counting sheep...' docs

---
+++ kata/8-kyu/counting-sheep-dot-dot-dot/index.test.ts
@@ -0,0 +1,34 @@
+import countSheeps from '.';
+
+describe('countSheeps', () => {
+  it('should count present sheep in array', () => {
+    expect(
+      countSheeps([
+        true,
+        true,
+        true,
+        false,
+        true,
+        true,
+        true,
+        true,
+        true,
+        false,
+        true,
+        false,
+        true,
+        false,
+        false,
+        true,
+        true,
+        true,
+        true,
+        true,
+        false,
+        false,
+        true,
+        true,
+      ]),
+    ).toEqual(17);
+  });
+});
