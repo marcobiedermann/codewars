f9254d4a059f665e1ebc716e51ef6394bd008222
Marco Biedermann <marco.biedermann@gmx.de>
2020-02-20 10:34:03 +0100

    refactor: 'Basic Mathematical Operations'
    Refactor 'Basic Mathematical Operations' by using hashmap to call operation

--- kata/8-kyu/basic-mathematical-operations/index.ts
+++ kata/8-kyu/basic-mathematical-operations/index.ts
@@ -1,22 +1,30 @@
 type Operation = '+' | '-' | '*' | '/';
 
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const operations = {
+  '+': add,
+  '-': subtract,
+  '*': multiply,
+  '/': divide,
+};
+
 function basicOp(operation: Operation, value1: number, value2: number): number {
-  switch (operation) {
-    case '+':
-      return value1 + value2;
-
-    case '-':
-      return value1 - value2;
-
-    case '*':
-      return value1 * value2;
-
-    case '/':
-      return value1 / value2;
-
-    default:
-      return 0;
-  }
+  return operations[operation](value1, value2);
 }
 
 export default basicOp;
