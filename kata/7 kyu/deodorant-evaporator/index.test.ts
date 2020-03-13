44ef49ded7f31d9a9a47b4eade74ec5970773a87
Marco Biedermann <marco.biedermann@gmx.de>
2020-03-13 08:55:51 +0100

    feat: 'Deodorant Evaporator'
    Add 'Deodorant Evaporator' kata

---
+++ kata/7 kyu/deodorant-evaporator/index.test.ts
@@ -0,0 +1,9 @@
+import evaporator from '.';
+
+describe('evaporator', () => {
+  it('should calculate day on which the vaporator will be out of use', () => {
+    expect.assertions(1);
+
+    expect(evaporator(10, 10, 10)).toStrictEqual(22);
+  });
+});
---
+++ kata/7 kyu/deodorant-evaporator/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable camelcase */
+
+function evaporator(_content: number, evap_per_day: number, threshold: number): number {
+  return Math.ceil(Math.log(threshold / 100) / Math.log(1 - evap_per_day / 100));
+}
+
+export default evaporator;
