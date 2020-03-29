b0469c260793ff86dcb7f875708c9b7f66f72465
Marco Biedermann <marco.biedermann@gmx.de>
2020-03-29 22:15:18 +0200

    refactor: 'Are they the "same"?'
    Refactor 'Are they the "same"?'

--- kata/6 kyu/are-they-the-same/index.ts
+++ kata/6 kyu/are-they-the-same/index.ts
@@ -1,14 +1,16 @@
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
 function comp(array1: number[], array2: number[]): boolean {
-  if (!Array.isArray(array1) || !Array.isArray(array2)) {
+  if (!array1 || !array2) {
     return false;
   }
 
-  const sortedArray1 = array1.sort((a, b) => a - b);
-  const sortedArray2 = array2.sort((a, b) => a - b);
+  const sortedArray1 = array1.sort(subtract);
+  const sortedArray2 = array2.sort(subtract);
 
-  return sortedArray1
-    .map((element) => element ** 2)
-    .every((element, index) => element === sortedArray2[index]);
+  return sortedArray1.every((element, index) => element ** 2 === sortedArray2[index]);
 }
 
 export default comp;
