396ee8f85b8ce3b33d934fe700e36c4c5403ede2
Marco Biedermann <marco.biedermann@gmx.de>
2020-04-07 08:25:01 +0200

    feat: 'Take a Number And Sum Its Digits Raised To The Consecutive Powers'
    Add 'Take a Number And Sum Its Digits Raised To The Consecutive Powers' kata

---
+++ kata/6 kyu/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/index.test.ts
@@ -0,0 +1,15 @@
+import sumDigPow from '.';
+
+describe('sumDigPow', () => {
+  it('should return list of numbers which digits raised to their consecutive power is same as number', () => {
+    expect.assertions(7);
+
+    expect(sumDigPow(1, 10)).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    expect(sumDigPow(1, 100)).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 89]);
+    expect(sumDigPow(10, 100)).toStrictEqual([89]);
+    expect(sumDigPow(90, 100)).toStrictEqual([]);
+    expect(sumDigPow(90, 150)).toStrictEqual([135]);
+    expect(sumDigPow(50, 150)).toStrictEqual([89, 135]);
+    expect(sumDigPow(10, 150)).toStrictEqual([89, 135]);
+  });
+});
---
+++ kata/6 kyu/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/index.ts
@@ -0,0 +1,27 @@
+function intToDigits(int: number): number[] {
+  if (int < 10) {
+    return [int];
+  }
+
+  return [...intToDigits(Math.floor(int / 10)), int % 10];
+}
+
+function sumDigPow(a: number, b: number): number[] {
+  const result: number[] = [];
+
+  for (let i = a; i <= b; i += 1) {
+    const digits = intToDigits(i);
+
+    const sum = digits.reduce((accumulator, currentValue, index) => {
+      return accumulator + currentValue ** (index + 1);
+    }, 0);
+
+    if (sum === i) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+export default sumDigPow;
