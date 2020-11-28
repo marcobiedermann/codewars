d6f235c59ce7fb27899bcf67f563a26bde2416f8
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-28 18:45:16 +0100

    feat: 'Convert a linked list to a string'
    Add 'Convert a linked list to a string' kata

---
+++ kata/7 kyu/convert-a-linked-list-to-a-string/index.ts
@@ -0,0 +1,23 @@
+class Node {
+  data: number;
+
+  next: Node | null;
+
+  constructor(data: number, next: Node | null = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function stringify(list: Node | null): string {
+  if (!list) {
+    return 'null';
+  }
+
+  const { data, next } = list;
+
+  return `${data} -> ${stringify(next)}`;
+}
+
+export default stringify;
+export { Node };
