34d233f6167d016cf7e11ed7ed3cf8f30a46f5a3
Marco Biedermann <marco.biedermann@gmx.de>
2020-12-02 11:36:44 +0100

    refactor: 'Alphabetical Addition'
    Refactor 'Alphabetical Addition' tests

--- kata/7 kyu/alphabetical-addition/index.test.ts
+++ kata/7 kyu/alphabetical-addition/index.test.ts
@@ -4,18 +4,12 @@ describe('addLetters', () => {
   it('should add up letters to one letter', () => {
     expect.assertions(7);
 
-    const tests = [
-      [['a', 'b', 'c'], 'f'],
-      [['z'], 'z'],
-      [['a', 'b'], 'c'],
-      [['c'], 'c'],
-      [['z', 'a'], 'a'],
-      [['y', 'c', 'b'], 'd'],
-      [[], 'z'],
-    ];
-
-    tests.forEach((test) => {
-      expect(addLetters(...test[0])).toStrictEqual(test[1]);
-    });
+    expect(addLetters('a', 'b', 'c')).toStrictEqual('f');
+    expect(addLetters('z')).toStrictEqual('z');
+    expect(addLetters('a', 'b')).toStrictEqual('c');
+    expect(addLetters('c')).toStrictEqual('c');
+    expect(addLetters('z', 'a')).toStrictEqual('a');
+    expect(addLetters('y', 'c', 'b')).toStrictEqual('d');
+    expect(addLetters()).toStrictEqual('z');
   });
 });
