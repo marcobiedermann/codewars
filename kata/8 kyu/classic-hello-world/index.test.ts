f72ad118254cca7960cc6e0800ba1323103d064c
Marco Biedermann <marco.biedermann@gmx.de>
2021-01-20 19:47:18 +0100

    test: console.log
    Add console.log test cases

---
+++ kata/8 kyu/classic-hello-world/index.test.ts
@@ -0,0 +1,13 @@
+import Solution from '.';
+
+jest.spyOn(console, 'log').mockImplementation();
+
+describe('solution', () => {
+  it('should log `Hello World!`', () => {
+    expect.assertions(1);
+
+    Solution.main();
+
+    expect(console.log).toHaveBeenCalledWith('Hello World!');
+  });
+});
--- kata/8 kyu/training-js-number-1-create-your-first-js-function-and-print-helloworld/index.test.ts
+++ kata/8 kyu/training-js-number-1-create-your-first-js-function-and-print-helloworld/index.test.ts
@@ -1,9 +1,14 @@
 import helloWorld from '.';
 
+jest.spyOn(console, 'log').mockImplementation();
+
 describe('helloWorld', () => {
   it('should define function `helloWorld`', () => {
-    expect.assertions(1);
+    expect.assertions(2);
+
+    helloWorld();
 
     expect(helloWorld).toBeDefined();
+    expect(console.log).toHaveBeenCalledWith('Hello World!');
   });
 });
