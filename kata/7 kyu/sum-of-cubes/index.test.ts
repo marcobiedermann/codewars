edb1ff08d4f112aa27b80618ed3767d68a1dad75
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2021-10-11 20:18:11 +0200

    feat: 'Sum of Cubes'
    Add 'Sum of Cubes' kata

---
+++ kata/7 kyu/sum-of-cubes/index.test.ts
@@ -0,0 +1,14 @@
+import sumCubes from '.';
+
+describe('sumCubes', () => {
+  it('should sum the cubed values from 1 to n', () => {
+    expect.assertions(6);
+
+    expect(sumCubes(1)).toBe(1);
+    expect(sumCubes(2)).toBe(9);
+    expect(sumCubes(3)).toBe(36);
+    expect(sumCubes(4)).toBe(100);
+    expect(sumCubes(10)).toBe(3025);
+    expect(sumCubes(123)).toBe(58155876);
+  });
+});
