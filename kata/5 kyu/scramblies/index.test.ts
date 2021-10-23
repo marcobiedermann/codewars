37dba1261382c304f9a5217db1f28d8792ef8732
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2021-10-23 15:42:46 +0200

    feat: 'Scramblies'
    Add 'Scramblies' kata

---
+++ kata/5 kyu/scramblies/index.test.ts
@@ -0,0 +1,17 @@
+import scramble from '.';
+
+describe('scramble', () => {
+  it('should check if a portion of `str1` can be rearranged to match `str2`', () => {
+    expect.assertions(9);
+
+    expect(scramble('rkqodlw', 'world')).toBe(true);
+    expect(scramble('cedewaraaossoqqyt', 'codewars')).toBe(true);
+    expect(scramble('katas', 'steak')).toBe(false);
+    expect(scramble('scriptjava', 'javascript')).toBe(true);
+    expect(scramble('scriptingjava', 'javascript')).toBe(true);
+    expect(scramble('scriptsjava', 'javascripts')).toBe(true);
+    expect(scramble('jscripts', 'javascript')).toBe(false);
+    expect(scramble('aabbcamaomsccdd', 'commas')).toBe(true);
+    expect(scramble('scriptjavx', 'javascript')).toBe(false);
+  });
+});
