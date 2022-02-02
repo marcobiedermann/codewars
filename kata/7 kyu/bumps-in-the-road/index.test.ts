073e7ef1e530b4204bc6fce278dca9f88b1a849b
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-02-02 20:51:03 +0100

    feat: 'Bumps in the Road'
    Add 'Bumps in the Road' kata

---
+++ kata/7 kyu/bumps-in-the-road/index.test.ts
@@ -0,0 +1,11 @@
+import bump from '.';
+
+describe('bump', () => {
+  it('should check if your can make it home safely', () => {
+    expect.assertions(3);
+
+    expect(bump('n')).toBe('Woohoo!');
+    expect(bump('_nnnnnnn_n__n______nn__nn_nnn')).toBe('Car Dead');
+    expect(bump('______n___n_')).toBe('Woohoo!');
+  });
+});
