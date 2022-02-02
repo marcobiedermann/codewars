4aabbc911fc925bbea544e3e6062fd41835c587d
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-02-02 20:43:04 +0100

    feat: 'Grasshopper - Basic Function Fixer'
    Add kata 'Grasshopper - Basic Function Fixer'

---
+++ kata/8 kyu/grasshopper-basic-function-fixer/index.test.ts
@@ -0,0 +1,11 @@
+import addFive from '.';
+
+describe('addFive', () => {
+  it('should add `5` to the given number', () => {
+    expect.assertions(3);
+
+    expect(addFive(5)).toBe(10);
+    expect(addFive(0)).toBe(5);
+    expect(addFive(-5)).toBe(0);
+  });
+});
