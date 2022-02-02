59fcbb96c9565f155b4dd64c4c8646252241aa31
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-02-02 21:34:02 +0100

    feat: 'Data Reverse'
    Add 'Data Reverse' kata

---
+++ kata/6 kyu/data-reverse/index.test.ts
@@ -0,0 +1,20 @@
+import dataReverse from '.';
+
+describe('dataReverse', () => {
+  it('should reverse chunks of 8', () => {
+    expect.assertions(2);
+
+    expect(
+      dataReverse([
+        1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1,
+        0,
+      ]),
+    ).toStrictEqual([
+      1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+      1,
+    ]);
+    expect(dataReverse([0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1])).toStrictEqual([
+      0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0,
+    ]);
+  });
+});
