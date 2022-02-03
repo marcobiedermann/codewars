902c95311fbf09a5f127427fa1b2e8672185120c
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-02-03 15:22:25 +0100

    feat: 'Find the Remainder'
    Add 'Find the Remainder' kata

---
+++ kata/8 kyu/find-the-remainder/index.test.ts
@@ -0,0 +1,12 @@
+import remainder from '.';
+
+describe('remainder', () => {
+  it('should find the remainder', () => {
+    expect.assertions(4);
+
+    expect(remainder(17, 5)).toBe(2);
+    expect(remainder(13, 72)).toStrictEqual(remainder(72, 13));
+    expect(remainder(1, 0)).toBeNaN();
+    expect(remainder(0, 0)).toBeNaN();
+  });
+});
