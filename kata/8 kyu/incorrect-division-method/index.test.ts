46ac91f1f8bc5f680f8aff6da8a05ff74df40bd8
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-02-04 10:27:27 +0100

    feat: 'Incorrect division method'
    Add 'Incorrect division method' kata

---
+++ kata/8 kyu/incorrect-division-method/index.test.ts
@@ -0,0 +1,15 @@
+import solve from '.';
+
+describe('solve', () => {
+  it('should divide its first argument by its second', () => {
+    expect.assertions(7);
+
+    expect(solve(2, 1)).toBe(2);
+    expect(solve(2, 4)).toBe(0.5);
+    expect(solve(42, 0)).toStrictEqual(Infinity);
+    expect(solve(0, 8)).toBe(0);
+    expect(solve(7, 2)).toBe(3.5);
+    expect(solve(4.2, 1)).toBe(4.2);
+    expect(solve(9999, 11)).toBe(909);
+  });
+});
---
+++ kata/8 kyu/incorrect-division-method/readme.md
@@ -0,0 +1,9 @@
+# [Incorrect division method](https://www.codewars.com/kata/54d1c59aba326343c80000e7)
+
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+---
+
+## Tags
+
+- Bugs
