d4b44ed27f07f3c97d24d43dabf8598d098a20c2
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-02-05 14:43:21 +0100

    feat: 'Filter the number'
    Add 'Filter the number' kata

---
+++ kata/7 kyu/filter-the-number/index.test.ts
@@ -0,0 +1,11 @@
+import filterString from '.';
+
+describe('filterString', () => {
+  it('should extract number from string', () => {
+    expect.assertions(3);
+
+    expect(filterString('123')).toBe(123);
+    expect(filterString('a1b2c3')).toBe(123);
+    expect(filterString('aa1bb2cc3dd')).toBe(123);
+  });
+});
