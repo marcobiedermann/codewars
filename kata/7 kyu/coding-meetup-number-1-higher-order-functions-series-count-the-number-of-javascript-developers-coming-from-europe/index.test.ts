61cdb679e22c08b5e880e52c817f35f76c6493bb
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-02-17 19:33:05 +0100

    feat: 'Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe'
    Add 'Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe' kata

---
+++ kata/7 kyu/coding-meetup-number-1-higher-order-functions-series-count-the-number-of-javascript-developers-coming-from-europe/index.test.ts
@@ -0,0 +1,64 @@
+import countDevelopers from '.';
+
+describe('countDevelopers', () => {
+  it('should return the number of JavaScript developers from Europe', () => {
+    expect.assertions(2);
+
+    expect(
+      countDevelopers([
+        {
+          firstName: 'Noah',
+          lastName: 'M.',
+          country: 'Switzerland',
+          continent: 'Europe',
+          age: 19,
+          language: 'JavaScript',
+        },
+        {
+          firstName: 'Maia',
+          lastName: 'S.',
+          country: 'Tahiti',
+          continent: 'Oceania',
+          age: 28,
+          language: 'JavaScript',
+        },
+        {
+          firstName: 'Shufen',
+          lastName: 'L.',
+          country: 'Taiwan',
+          continent: 'Asia',
+          age: 35,
+          language: 'HTML',
+        },
+        {
+          firstName: 'Sumayah',
+          lastName: 'M.',
+          country: 'Tajikistan',
+          continent: 'Asia',
+          age: 30,
+          language: 'CSS',
+        },
+      ]),
+    ).toBe(1);
+    expect(
+      countDevelopers([
+        {
+          firstName: 'Oliver',
+          lastName: 'Q.',
+          country: 'Australia',
+          continent: 'Oceania',
+          age: 19,
+          language: 'HTML',
+        },
+        {
+          firstName: 'Lukas',
+          lastName: 'R.',
+          country: 'Austria',
+          continent: 'Europe',
+          age: 89,
+          language: 'HTML',
+        },
+      ]),
+    ).toBe(0);
+  });
+});
