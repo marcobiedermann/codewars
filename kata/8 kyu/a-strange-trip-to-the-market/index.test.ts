481564101ca621cdcb17ade2a35a2e423bf80bc2
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-04-20 09:25:25 +0200

    feat: 'A Strange Trip to the Market'
    Add 'A Strange Trip to the Market' kata

---
+++ kata/8 kyu/a-strange-trip-to-the-market/index.test.ts
@@ -0,0 +1,16 @@
+import isLockNessMonster from '.';
+
+describe('isLockNessMonster', () => {
+  it('should', () => {
+    expect.assertions(2);
+
+    expect(
+      isLockNessMonster('Your girlscout cookies are ready to ship. Your total comes to tree fiddy'),
+    ).toBe(true);
+    expect(
+      !isLockNessMonster(
+        'Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance.',
+      ),
+    ).toBe(true);
+  });
+});
