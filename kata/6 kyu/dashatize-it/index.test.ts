26515f6af71d30dcb1719a3c95d916ff8abf2101
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-04-21 16:49:08 +0200

    feat: 'Dashatize it'
    Add 'Dashatize it' kata

---
+++ kata/6 kyu/dashatize-it/index.test.ts
@@ -0,0 +1,16 @@
+import dashatize from '.';
+
+describe('dashatize', () => {
+  it('should place a dash between each odd number', () => {
+    expect.assertions(8);
+
+    expect(dashatize(274)).toBe('2-7-4');
+    expect(dashatize(5311)).toBe('5-3-1-1');
+    expect(dashatize(86320)).toBe('86-3-20');
+    expect(dashatize(974302)).toBe('9-7-4-3-02');
+    expect(dashatize(NaN)).toBe('NaN');
+    expect(dashatize(0)).toBe('0');
+    expect(dashatize(-1)).toBe('1');
+    expect(dashatize(-28369)).toBe('28-3-6-9');
+  });
+});
