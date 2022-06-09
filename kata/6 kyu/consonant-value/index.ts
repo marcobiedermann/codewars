94b4e4845e8b82c96308ab001181eaff46c8a552
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-06-09 09:00:53 +0200

    refactor: 'Consonant value'
    Refactor 'Consonant value' kata

--- kata/6 kyu/consonant-value/index.ts
+++ kata/6 kyu/consonant-value/index.ts
@@ -1,12 +1,23 @@
 function solve(s: string): number {
-  return Math.max(
-    ...s.split(/[aeiou]/g).map((slice) =>
-      slice
-        .split('')
-        .map((character) => character.charCodeAt(0) - 96)
-        .reduce((accumulator, currentValue) => accumulator + currentValue, 0),
-    ),
-  );
+  let max = 0;
+  let currentMax = 0;
+
+  for (let i = 0; i < s.length; i += 1) {
+    const current = s[i];
+    const charCode = current.charCodeAt(0) - 96;
+
+    if (current.match(/[aeiou]/g)) {
+      currentMax = 0;
+    } else {
+      currentMax += charCode;
+    }
+
+    if (currentMax > max) {
+      max = currentMax;
+    }
+  }
+
+  return max;
 }
 
 export default solve;
