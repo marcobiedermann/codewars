97a1af8e1ecb36eb453104382fbd4665bc8a9498
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-06-24 16:23:22 +0200

    feat: 'Compare within margin'
    Add 'Compare within margin' kata

---
+++ kata/8 kyu/compare-within-margin/index.test.ts
@@ -0,0 +1,16 @@
+import closeCompare from '.';
+
+describe('closeCompare', () => {
+  it('should', () => {
+    expect.assertions(7);
+
+    expect(closeCompare(4, 5)).toStrictEqual(-1);
+    expect(closeCompare(5, 5)).toBe(0);
+    expect(closeCompare(6, 5)).toBe(1);
+    expect(closeCompare(-6, -5)).toStrictEqual(-1);
+
+    expect(closeCompare(2, 5, 3)).toBe(0);
+    expect(closeCompare(8.1, 5, 3)).toBe(1);
+    expect(closeCompare(1.99, 5, 3)).toStrictEqual(-1);
+  });
+});
