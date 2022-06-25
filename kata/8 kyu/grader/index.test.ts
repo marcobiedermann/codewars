71f640973e30161c3fa907e73883f3dc9782f76c
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-06-25 10:25:19 +0200

    feat: 'Grader'
    Add 'Grader' kata

---
+++ kata/8 kyu/grader/index.test.ts
@@ -0,0 +1,14 @@
+import grader from '.';
+
+describe('grader', () => {
+  it('should return a grade based on the score', () => {
+    expect.assertions(6);
+
+    expect(grader(0)).toBe('F');
+    expect(grader(1.1)).toBe('F');
+    expect(grader(0.9)).toBe('A');
+    expect(grader(0.8)).toBe('B');
+    expect(grader(0.7)).toBe('C');
+    expect(grader(0.6)).toBe('D');
+  });
+});
