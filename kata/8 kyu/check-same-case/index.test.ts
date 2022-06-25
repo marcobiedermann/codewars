e23818416462c72877b006d0472a3dc0b024b666
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-06-25 20:08:53 +0200

    feat: 'Check same case'
    Add 'Check same case' kata

---
+++ kata/8 kyu/check-same-case/index.test.ts
@@ -0,0 +1,16 @@
+import sameCase from '.';
+
+describe('sameCase', () => {
+  it('should check if two given characters are the same case', () => {
+    expect.assertions(8);
+
+    expect(sameCase('C', 'B')).toBe(1);
+    expect(sameCase('b', 'a')).toBe(1);
+    expect(sameCase('d', 'd')).toBe(1);
+    expect(sameCase('A', 's')).toBe(0);
+    expect(sameCase('c', 'B')).toBe(0);
+    expect(sameCase('b', 'Z')).toBe(0);
+    expect(sameCase('\t', 'Z')).toStrictEqual(-1);
+    expect(sameCase('H', ':')).toStrictEqual(-1);
+  });
+});
