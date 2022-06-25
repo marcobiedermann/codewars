d7ca9344a51787c5a0e99797373ccea82c827a0c
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-06-25 10:25:19 +0200

    feat: 'Grader'
    Add 'Grader' kata

---
+++ kata/8 kyu/grader/index.ts
@@ -0,0 +1,27 @@
+type Grade = 'A' | 'B' | 'C' | 'D' | 'F';
+
+function grader(score: number): Grade {
+  if (score > 1) {
+    return 'F';
+  }
+
+  if (score >= 0.9) {
+    return 'A';
+  }
+
+  if (score >= 0.8) {
+    return 'B';
+  }
+
+  if (score >= 0.7) {
+    return 'C';
+  }
+
+  if (score >= 0.6) {
+    return 'D';
+  }
+
+  return 'F';
+}
+
+export default grader;
