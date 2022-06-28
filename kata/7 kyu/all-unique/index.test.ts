251367119e649a4d42b2f814384662d5ae8f18c5
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-06-28 09:01:17 +0200

    feat: 'All unique'
    Add 'All unique' kata

---
+++ kata/7 kyu/all-unique/index.test.ts
@@ -0,0 +1,12 @@
+import hasUniqueChars from '.';
+
+describe('hasUniqueChars', () => {
+  it('should check if a string contains only unique characters', () => {
+    expect.assertions(4);
+
+    expect(hasUniqueChars('  nAa')).toBe(false);
+    expect(hasUniqueChars('abcdef')).toBe(true);
+    expect(hasUniqueChars('aA')).toBe(true);
+    expect(hasUniqueChars('++-')).toBe(false);
+  });
+});
