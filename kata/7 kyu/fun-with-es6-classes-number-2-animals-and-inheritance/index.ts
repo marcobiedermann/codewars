d3b83a20ba81d6e2aa0f8b7ece93b3ccf124edcd
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-07-01 16:03:07 +0200

    feat: 'Fun with ES6 Classes #2 - Animals and Inheritance'
    Add 'Fun with ES6 Classes #2 - Animals and Inheritance' kata

---
+++ kata/7 kyu/fun-with-es6-classes-number-2-animals-and-inheritance/index.ts
@@ -0,0 +1,52 @@
+/* eslint-disable max-classes-per-file */
+
+class Animal {
+  constructor(
+    public name: string,
+    public age: number,
+    public legs: number,
+    public species: string,
+    public status: string,
+  ) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+class Shark extends Animal {
+  constructor(name: string, age: number, status: string) {
+    super(name, age, 0, 'shark', status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name: string, age: number, status: string) {
+    super(name, age, 4, 'cat', status);
+  }
+
+  introduce(): string {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name: string, age: number, status: string, private master: string) {
+    super(name, age, 4, 'dog', status);
+
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
+
+export default Animal;
+export { Shark, Cat, Dog };
