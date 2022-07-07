5bc5e27558275d77dfc86670be70cf41b5302ef9
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-07-07 13:06:37 +0200

    feat: 'Object Oriented Piracy'
    Add 'Object Oriented Piracy' kata

---
+++ kata/8 kyu/object-oriented-piracy/index.test.ts
@@ -0,0 +1,11 @@
+import Ship from '.';
+
+describe('ship', () => {
+  it('should check if the ship is worth it', () => {
+    expect.assertions(1);
+
+    const emptyShip = new Ship(0, 0);
+
+    expect(emptyShip.isWorthIt()).toBe(false);
+  });
+});
---
+++ kata/8 kyu/object-oriented-piracy/index.ts
@@ -0,0 +1,12 @@
+class Ship {
+  constructor(private draft: number, private crew: number) {
+    this.draft = draft;
+    this.crew = crew;
+  }
+
+  isWorthIt(): boolean {
+    return this.draft - this.crew * 1.5 > 20;
+  }
+}
+
+export default Ship;
