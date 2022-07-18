d32891446395e1bae634e58daea1b5dafd148642
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-07-18 12:54:14 +0200

    feat: 'Type of sum'
    Add 'Type of sum' kata

---
+++ kata/8 kyu/type-of-sum/index.test.ts
@@ -0,0 +1,20 @@
+import typeOfSum from '.';
+
+describe('typeOfSum', () => {
+  it('should', () => {
+    expect.assertions(12);
+
+    expect(typeOfSum(12, 1)).toBe('number');
+    expect(typeOfSum('d', 1)).toBe('string');
+    expect(typeOfSum(1, 'a')).toBe('string');
+    expect(typeOfSum('dd', '')).toBe('string');
+    expect(typeOfSum(true, 1)).toBe('number');
+    expect(typeOfSum('s', false)).toBe('string');
+    expect(typeOfSum(null, 1)).toBe('number');
+    expect(typeOfSum('s', null)).toBe('string');
+    expect(typeOfSum(null, undefined)).toBe('number');
+    expect(typeOfSum(undefined, 're')).toBe('string');
+    expect(typeOfSum(undefined, true)).toBe('number');
+    expect(typeOfSum(null, false)).toBe('number');
+  });
+});
---
+++ kata/8 kyu/type-of-sum/readme.md
@@ -0,0 +1,9 @@
+# [Type of sum](https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba)
+
+Return the type of the sum of the two arguments
+
+---
+
+## Tags
+
+- Fundamentals
