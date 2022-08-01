d0211fafa13278f39f24d4b1a887e4928ec19f75
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-08-01 09:29:09 +0200

    feat: 'Price of Mangoes'
    Add 'Price of Mangoes' kata

---
+++ kata/8 kyu/price-of-mangoes/index.test.ts
@@ -0,0 +1,10 @@
+import mango from '.';
+
+describe('mango', () => {
+  it('should return the total cost of the mangoes', () => {
+    expect.assertions(2);
+
+    expect(mango(3, 3)).toBe(6);
+    expect(mango(9, 5)).toBe(30);
+  });
+});
