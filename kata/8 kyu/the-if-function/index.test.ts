e10f43227450437278bf4fb5208b9e5793b0a447
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-08-01 09:07:03 +0200

    chore: Dependencies
    Update Node dependencies

--- kata/8 kyu/the-if-function/index.test.ts
+++ kata/8 kyu/the-if-function/index.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import _if from '.';
 
 jest.spyOn(console, 'log').mockImplementation();
--- kata/8 kyu/training-js-number-1-create-your-first-js-function-and-print-helloworld/index.test.ts
+++ kata/8 kyu/training-js-number-1-create-your-first-js-function-and-print-helloworld/index.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import helloWorld from '.';
 
 jest.spyOn(console, 'log').mockImplementation();
