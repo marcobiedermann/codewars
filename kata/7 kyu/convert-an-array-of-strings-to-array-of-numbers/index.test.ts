c8aac9733a70d7d29b39d25dd7523aa1b1e6b413
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-08-20 21:49:34 +0200

    feat: 'Convert an array of strings to array of numbers'
    Add 'Convert an array of strings to array of numbers' kata

---
+++ kata/7 kyu/convert-an-array-of-strings-to-array-of-numbers/index.test.ts
@@ -0,0 +1,9 @@
+import toNumberArray from '.';
+
+describe('toNumberArray', () => {
+  it('should convert convert an array of strings to an array of numbers', () => {
+    expect.assertions(1);
+
+    expect(toNumberArray(['1.1', '2.2', '3.3'])).toStrictEqual([1.1, 2.2, 3.3]);
+  });
+});
