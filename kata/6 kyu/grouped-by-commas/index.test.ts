1ee385b69eeb675abf2d5232b73761124fbe6658
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-08-22 15:13:18 +0200

    feat: 'Grouped by commas'
    Add 'Grouped by commas' kata

---
+++ kata/6 kyu/grouped-by-commas/index.test.ts
@@ -0,0 +1,16 @@
+import groupByCommas from '.';
+
+describe('groupByCommas', () => {
+  it('should', () => {
+    expect.assertions(8);
+
+    expect(groupByCommas(1)).toBe('1');
+    expect(groupByCommas(10)).toBe('10');
+    expect(groupByCommas(100)).toBe('100');
+    expect(groupByCommas(1000)).toBe('1,000');
+    expect(groupByCommas(10000)).toBe('10,000');
+    expect(groupByCommas(100000)).toBe('100,000');
+    expect(groupByCommas(1000000)).toBe('1,000,000');
+    expect(groupByCommas(35235235)).toBe('35,235,235');
+  });
+});
