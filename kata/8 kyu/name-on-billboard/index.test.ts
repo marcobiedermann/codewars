e37d59062397eb38c4fd506192313eb552d9ddce
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-09-12 09:19:46 +0200

    feat: 'Name on billboard'
    Add 'Name on billboard' kata

---
+++ kata/8 kyu/name-on-billboard/index.test.ts
@@ -0,0 +1,19 @@
+import billboard from '.';
+
+describe('billboard', () => {
+  it('should calculate the billboard ad cost', () => {
+    expect.assertions(11);
+
+    expect(billboard('Jeong-Ho Aristotelis')).toBe(600);
+    expect(billboard('Abishai Charalampos')).toBe(570);
+    expect(billboard('Idwal Augustin')).toBe(420);
+    expect(billboard('Hadufuns John', 20)).toBe(260);
+    expect(billboard('Zoroaster Donnchadh')).toBe(570);
+    expect(billboard('Claude Miljenko')).toBe(450);
+    expect(billboard('Werner Vígi', 15)).toBe(165);
+    expect(billboard('Anani Fridumar')).toBe(420);
+    expect(billboard('Paolo Oli')).toBe(270);
+    expect(billboard('Hjalmar Liupold', 40)).toBe(600);
+    expect(billboard('Simon Eadwulf')).toBe(390);
+  });
+});
