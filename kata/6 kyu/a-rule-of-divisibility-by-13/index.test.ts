cec6fcad69968d5ca150b48d3d150c2b68830071
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-09-13 09:42:33 +0200

    feat: 'A Rule of Divisibility by 13'
    Add 'A Rule of Divisibility by 13' kata

---
+++ kata/6 kyu/a-rule-of-divisibility-by-13/index.test.ts
@@ -0,0 +1,13 @@
+import thirt from '.';
+
+describe('thirt', () => {
+  it('should get the remainder when deviding by 13', () => {
+    expect.assertions(5);
+
+    expect(thirt(8529)).toBe(79);
+    expect(thirt(85299258)).toBe(31);
+    expect(thirt(5634)).toBe(57);
+    expect(thirt(1111111111)).toBe(71);
+    expect(thirt(987654321)).toBe(30);
+  });
+});
