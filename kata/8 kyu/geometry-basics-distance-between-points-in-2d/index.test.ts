b63cfb50c9eb4b67e93595c944bc03611df5e49c
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-09-14 09:50:10 +0200

    feat: 'Geometry Basics: Distance between points in 2D'
    Add 'Geometry Basics: Distance between points in 2D' kata

---
+++ kata/8 kyu/geometry-basics-distance-between-points-in-2d/index.test.ts
@@ -0,0 +1,13 @@
+import distanceBetweenPoints, { Point } from '.';
+
+describe('distanceBetweenPoints', () => {
+  it('should return the distance between two points', () => {
+    expect.assertions(3);
+
+    expect(distanceBetweenPoints(new Point(3, 3), new Point(3, 3))).toBe(0);
+    expect(distanceBetweenPoints(new Point(1, 6), new Point(4, 2))).toBe(5);
+    expect(distanceBetweenPoints(new Point(-10.2, 12.5), new Point(0.3, 14.7)).toFixed(6)).toBe(
+      '10.728001',
+    );
+  });
+});
---
+++ kata/8 kyu/geometry-basics-distance-between-points-in-2d/readme.md
@@ -0,0 +1,16 @@
+# [Geometry Basics: Distance between points in 2D](https://www.codewars.com/kata/58dced7b702b805b200000be)
+
+This series of katas will introduce you to basics of doing geometry with computers.
+
+`Point` objects have `x` and `y` attributes (`X` and `Y` in `C#`) attributes.
+
+Write a function calculating distance between `Point a` and `Point b`.
+
+Tests round answers to 6 decimal places.
+
+---
+
+## Tags
+
+- Fundamentals
+- Geometry
