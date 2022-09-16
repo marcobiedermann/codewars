bdcbb188136547f8294293a2dd9a529983be603e
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-09-16 21:27:53 +0200

    feat: 'FIXME: Get Full Name'
    Add 'FIXME: Get Full Name' kata

---
+++ kata/7 kyu/fixme-get-full-name/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-useless-constructor */
+
+class Dinglemouse {
+  constructor(private firstName: string, private lastName: string) {}
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`.trim();
+  }
+}
+
+export default Dinglemouse;
