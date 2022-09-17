62601f92bcdd416f28d26b191411f32a3a091830
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-09-17 21:25:49 +0200

    feat: 'Elapsed Seconds'
    Add 'Elapsed Seconds' kata

---
+++ kata/7 kyu/elapsed-seconds/index.test.ts
@@ -0,0 +1,11 @@
+import elapsedSeconds from '.';
+
+describe('elapsedSeconds', () => {
+  it('should return the number of seconds elapsed between the start and end time', () => {
+    expect.assertions(3);
+
+    expect(elapsedSeconds(new Date(2013, 1, 1, 0, 0, 1), new Date(2013, 1, 1, 0, 0, 2))).toBe(1);
+    expect(elapsedSeconds(new Date(2013, 1, 1, 0, 0, 1), new Date(2013, 1, 1, 0, 0, 20))).toBe(19);
+    expect(elapsedSeconds(new Date(2013, 1, 1, 0, 0, 1), new Date(2013, 1, 1, 0, 1, 20))).toBe(79);
+  });
+});
