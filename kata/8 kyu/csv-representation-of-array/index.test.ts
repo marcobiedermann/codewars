b7429b5b9f209cd315a4496e591f341b03c6b9e0
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-09-17 15:01:00 +0200

    feat: 'CSV representation of array'
    Add 'CSV representation of array' kata

---
+++ kata/8 kyu/csv-representation-of-array/index.test.ts
@@ -0,0 +1,31 @@
+import toCsvText from '.';
+
+describe('toCsvText', () => {
+  it('should return the CSV representation of a two-dimensional array', () => {
+    expect.assertions(3);
+
+    expect(
+      toCsvText([
+        [0, 1, 2, 3, 45],
+        [10, 11, 12, 13, 14],
+        [20, 21, 22, 23, 24],
+        [30, 31, 32, 33, 34],
+      ]),
+    ).toBe('0,1,2,3,45\n10,11,12,13,14\n20,21,22,23,24\n30,31,32,33,34');
+
+    expect(
+      toCsvText([
+        [-25, 21, 2, -33, 48],
+        [30, 31, -32, 33, -34],
+      ]),
+    ).toBe('-25,21,2,-33,48\n30,31,-32,33,-34');
+
+    expect(
+      toCsvText([
+        [5, 55, 5, 5, 55],
+        [6, 6, 66, 23, 24],
+        [666, 31, 66, 33, 7],
+      ]),
+    ).toBe('5,55,5,5,55\n6,6,66,23,24\n666,31,66,33,7');
+  });
+});
---
+++ kata/8 kyu/csv-representation-of-array/index.ts
@@ -0,0 +1,5 @@
+function toCsvText(array: number[][]): string {
+  return array.join('\n');
+}
+
+export default toCsvText;
