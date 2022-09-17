08c6e240dd8c138dbd3d815a20b27943ebea9b6e
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-09-17 21:09:03 +0200

    feat: 'Regexp Basics - is it a vowel?'
    Add 'Regexp Basics - is it a vowel?' kata

---
+++ kata/7 kyu/regexp-basics-is-it-a-vowel/index.test.ts
@@ -0,0 +1,14 @@
+import vowel from '.';
+
+describe('vowel', () => {
+  it('should check if the string is a vowel', () => {
+    expect.assertions(6);
+
+    expect(vowel('')).toBe(false);
+    expect(vowel('a')).toBe(true);
+    expect(vowel('E')).toBe(true);
+    expect(vowel('ou')).toBe(false);
+    expect(vowel('z')).toBe(false);
+    expect(vowel('lol')).toBe(false);
+  });
+});
---
+++ kata/7 kyu/regexp-basics-is-it-a-vowel/readme.md
@@ -0,0 +1,10 @@
+# [Regexp Basics - is it a vowel?](https://www.codewars.com/kata/567bed99ee3451292c000025)
+
+Implement the function which should return `true` if given object is a vowel (meaning `a, e, i, o, u`, uppercase or lowercase), and `false` otherwise.
+
+---
+
+## Tags
+
+- Fundamentals
+- Regular Expressions
