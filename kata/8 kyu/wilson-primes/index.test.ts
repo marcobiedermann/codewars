a7682fbef561b12bbc4331c91eebbd778ae6cfd1
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-09-26 09:29:22 +0200

    feat: 'Wilson primes' (#373)
    Add 'Wilson primes' kata

---
+++ kata/8 kyu/wilson-primes/index.test.ts
@@ -0,0 +1,11 @@
+import amIWilson from '.';
+
+describe('amIWilson', () => {
+  it('should check if number is a Wilson prime', () => {
+    expect.assertions(3);
+
+    expect(amIWilson(5)).toBe(true);
+    expect(amIWilson(9)).toBe(false);
+    expect(amIWilson(6)).toBe(false);
+  });
+});
