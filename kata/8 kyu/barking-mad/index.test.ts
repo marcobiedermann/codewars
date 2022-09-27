459cf4d0776974ed61fbf5a02b714ea9ab022ea5
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-09-27 16:04:49 +0200

    feat: 'Barking mad' (#374)
    Add 'Barking mad' kata

---
+++ kata/8 kyu/barking-mad/index.test.ts
@@ -0,0 +1,10 @@
+import { scoobydoo, snoopy } from '.';
+
+describe('dog', () => {
+  it('should bark', () => {
+    expect.assertions(2);
+
+    expect(snoopy.bark()).toBe('Woof');
+    expect(scoobydoo.bark()).toBe('Woof');
+  });
+});
