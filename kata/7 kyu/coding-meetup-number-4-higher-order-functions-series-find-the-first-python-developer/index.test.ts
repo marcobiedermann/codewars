79a7444d58c885d83450e75262137f22415a3285
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-10-02 17:43:42 +0200

    feat: 'Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer'
    Add 'Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer' kata

---
+++ kata/7 kyu/coding-meetup-number-4-higher-order-functions-series-find-the-first-python-developer/index.test.ts
@@ -0,0 +1,56 @@
+import getFirstPython from '.';
+
+describe('getFirstPython', () => {
+  it('should find the first Python developer', () => {
+    expect.assertions(2);
+
+    expect(
+      getFirstPython([
+        {
+          firstName: 'Mark',
+          lastName: 'G.',
+          country: 'Scotland',
+          continent: 'Europe',
+          age: 22,
+          language: 'JavaScript',
+        },
+        {
+          firstName: 'Victoria',
+          lastName: 'T.',
+          country: 'Puerto Rico',
+          continent: 'Americas',
+          age: 30,
+          language: 'Python',
+        },
+        {
+          firstName: 'Emma',
+          lastName: 'B.',
+          country: 'Norway',
+          continent: 'Europe',
+          age: 19,
+          language: 'Clojure',
+        },
+      ]),
+    ).toBe('Victoria, Puerto Rico');
+    expect(
+      getFirstPython([
+        {
+          firstName: 'Kseniya',
+          lastName: 'T.',
+          country: 'Belarus',
+          continent: 'Europe',
+          age: 29,
+          language: 'JavaScript',
+        },
+        {
+          firstName: 'Amar',
+          lastName: 'V.',
+          country: 'Bosnia and Herzegovina',
+          continent: 'Europe',
+          age: 32,
+          language: 'Ruby',
+        },
+      ]),
+    ).toBe('There will be no Python developers');
+  });
+});
