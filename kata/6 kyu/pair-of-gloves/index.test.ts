d128a3e0f42ee0588f53a1b7d2e74f0cc69c2a9c
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-10-20 12:16:17 +0200

    feat: 'Pair of gloves'
    Add 'Pair of gloves' kata

---
+++ kata/6 kyu/pair-of-gloves/index.test.ts
@@ -0,0 +1,11 @@
+import numberOfPairs from '.';
+
+describe('numberOfPairs', () => {
+  it('should return the number of pairs', () => {
+    expect.assertions(3);
+
+    expect(numberOfPairs(['red', 'red'])).toBe(1);
+    expect(numberOfPairs(['red', 'green', 'blue'])).toBe(0);
+    expect(numberOfPairs(['gray', 'black', 'purple', 'purple', 'gray', 'black'])).toBe(3);
+  });
+});
