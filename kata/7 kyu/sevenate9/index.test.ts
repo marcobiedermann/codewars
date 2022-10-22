cbad1e5662c9ca7109793056772b782f1ce9a865
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-10-22 22:23:04 +0200

    feat: 'SevenAte9'
    Add 'SevenAte9' kata

---
+++ kata/7 kyu/sevenate9/index.test.ts
@@ -0,0 +1,11 @@
+import sevenAte9 from '.';
+
+describe('sevenAte9', () => {
+  it('should remove evert `9` that is in between `7`s', () => {
+    expect.assertions(3);
+
+    expect(sevenAte9('797')).toBe('77');
+    expect(sevenAte9('7979797')).toBe('7777');
+    expect(sevenAte9('165561786121789797')).toBe('16556178612178977');
+  });
+});
---
+++ kata/7 kyu/sevenate9/readme.md
@@ -0,0 +1,16 @@
+# [SevenAte9](https://www.codewars.com/kata/559f44187fa851efad000087)
+
+Write a function that removes every lone `9` that is inbetween `7`s.
+
+```javascript
+"79712312" --> "7712312"
+"79797"    --> "777"
+```
+
+---
+
+## Tags
+
+- Fundamentals
+- Logic
+- Strings
