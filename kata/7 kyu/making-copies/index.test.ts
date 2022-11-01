6eed9efdfb61f7180e9ec47477366823236b5069
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-11-01 09:39:22 +0100

    feat: 'Making Copies'
    Add 'Making Copies' kata

---
+++ kata/7 kyu/making-copies/index.test.ts
@@ -0,0 +1,14 @@
+import copyList from '.';
+
+describe('copyList', () => {
+  it('should create a copy', () => {
+    expect.assertions(2);
+
+    const t = [1, 2, 3, 4];
+    const tCopy = copyList(t);
+
+    expect(t).toStrictEqual(tCopy);
+    t[1] += 5;
+    expect(t).not.toStrictEqual(tCopy);
+  });
+});
