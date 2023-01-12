b4a7f1f1f91b79d67fc7238b079b137d2d1b593c
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-01-12 09:30:51 +0100

    feat: "Find Count of Most Frequent Item in an Array"
    Add "Find Count of Most Frequent Item in an Array" kata

---
+++ kata/7 kyu/find-count-of-most-frequent-item-in-an-array/index.test.ts
@@ -0,0 +1,10 @@
+import mostFrequentItemCount from '.';
+
+describe('mostFrequentItemCount', () => {
+  it('should find the count of the most frequent item of an array', () => {
+    expect.assertions(2);
+
+    expect(mostFrequentItemCount([3, -1, -1])).toBe(2);
+    expect(mostFrequentItemCount([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3])).toBe(5);
+  });
+});
